7b96c8d7c369a5f2d3e68f448c15996ce14ded89
TheBestPessimist <cristian@tbp.land>
2019-03-18 17:46:44 +0200

    Ignore some more windows stuff

--- welcome to windows 10.ps1
+++ welcome to windows 10.ps1
@@ -18,12 +18,28 @@ Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer
 DISM /online /disable-feature /FeatureName:MSRDC-Infrastructure
 
 
-#Disable xBox services - "xBox Game Monitoring Service" - XBGM - Can't be disabled (access denied)
+# Disable xBox services - "xBox Game Monitoring Service" - XBGM - Can't be disabled (access denied)
 Get-Service XblAuthManager,XblGameSave,XboxNetApiSvc -erroraction silentlycontinue | stop-service -passthru | set-service -startuptype disabled
 
 
 # Disable some windows Activity tracker
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\System\' -Value 0 -Name 'PublishUserActivities'
+
+# Disable Windows Welcome Experience
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager' -Value 0 -Name 'SubscribedContent-310093Enabled'
+
+# Disable Windows Get tips, tricks, and suggestions as you use Windows
+Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager' -Value 0 -Name 'SubscribedContent-338389Enabled'
+
+# Disable Windows Snap Assist (show what can be snapped next to this window)
+# Ref: https://www.tenforums.com/tutorials/4343-turn-off-snap-windows-windows-10-a.html#option3
+Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Value 0 -Name 'SnapAssist'
+
+# Disable AutoPlay of USB devices
+Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\AutoplayHandlers' -Value 1 -Name 'DisableAutoplay'
+
+# Disable app suggestions from Start menu
+Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager' -Value 0 -Name 'SubscribedContent-338388Enabled'
 
 
 # Autologin windows
