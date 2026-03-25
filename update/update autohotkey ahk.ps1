73375737adac1874aab7eb68951ca748662ec540
TheBestPessimist <cristian@tbp.land>
2026-03-25 10:25:59 +0200

    Update ahk script

--- update/update autohotkey ahk.ps1
+++ update/update autohotkey ahk.ps1
@@ -21,8 +21,19 @@ Write-Host "Downloaded to: $fileName"
 # Remove old ahk folder if it exists
 Remove-Item "ahk" -Recurse -Force
 
+# Unzip
 & "C:\Program Files\7-Zip\7z.exe" x $fileName -o"ahk" -y
 
+# Remove things i don't need
+Remove-Item ahk/AutoHotkey32.exe
+Remove-Item ahk/license.txt
 
 
-explorer ahk
+# Kill ahk if running
+Get-Process -Name "AutoHotkey64" -ErrorAction SilentlyContinue | Stop-Process -Force
+
+# Copy files
+robocopy /E /Z /R:5 /W:5 /TBD /unicode /V /XJ /ETA /COPY:DT /DCOPY:DT /MT:32 'ahk' 'D:/all/all/AutoHotkey/'
+
+# Restart ahk
+Start-Process "D:/all/all/AutoHotkey/AutoHotkey64.exe"
