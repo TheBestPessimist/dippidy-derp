f13c79830189159c402199860ae939f3f2433428
TheBestPessimist <cristian@tbp.land>
2026-03-25 10:19:01 +0200

    Update ahk script

--- update/update autohotkey ahk.ps1
+++ update/update autohotkey ahk.ps1
@@ -6,9 +6,8 @@ $repo = "AutoHotkey/AutoHotkey"
 $apiUrl = "https://api.github.com/repos/$repo/releases/latest"
 $release = Invoke-RestMethod -Uri $apiUrl
 
-# Find the asset matching "mpv-dev-x86_64-v3"
+# Find the asset
 $asset = $release.assets | Where-Object { $_.name -like "*.zip" }
-
 
 # Download the file
 $downloadUrl = $asset.browser_download_url
@@ -19,6 +18,11 @@ Write-Host "Downloading $fileName..."
 Invoke-WebRequest -Uri $downloadUrl -OutFile $fileName
 Write-Host "Downloaded to: $fileName"
 
+# Remove old ahk folder if it exists
+Remove-Item "ahk" -Recurse -Force
+
 & "C:\Program Files\7-Zip\7z.exe" x $fileName -o"ahk" -y
+
+
 
 explorer ahk
